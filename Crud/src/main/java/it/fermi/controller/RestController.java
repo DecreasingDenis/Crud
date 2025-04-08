7f4388292563f89b8b04d5b87c1d8ad86e4740ce
Denis <128802002+DecreasingDenis@users.noreply.github.com>
2025-04-08 15:42:10 +0200

    push Crud
    Push dell'intera cartella IntelliJ di Crud (potrei sovrapporre i file più avanti)

---
+++ Crud/src/main/java/it/fermi/controller/RestController.java
@@ -0,0 +1,21 @@
+package it.fermi.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@org.springframework.web.bind.annotation.RestController     // Short for @Controller + @ResponseBody
+
+public class RestController {
+
+    // Gestisce richiesta GET di /api/richiestaConParametri/hello
+    @GetMapping("/richiestaConParametri/{param1}")
+    public String richiestaConParametri(@PathVariable String param1, @RequestParam(required = false) String param2) {
+        return param1 + " " + param2;  // Echoes la variabile del path come risposta
+    }
+
+    @GetMapping("/richiestaPostConParametriNumerici/{param1}/{param2}")
+    public String richiestaPostConParametri(@PathVariable int param1, @PathVariable int param2) {
+        int sum = param1 + param2;
+        return "somma: " + sum;
+    }
+
+}
